a9aaa7bc69fb2478bb2dd681bdf85a629dbf5e23
Tumar <115883559+tumar23kan@users.noreply.github.com>
2022-10-27 22:56:57 +0600

    Add files via upload

---
+++ numbers.js
@@ -0,0 +1,128 @@
+// Data types-...
+// 8 types  бул 2 ге болунот
+
+//1-примитивный простой(primitive)
+// number - число
+// string - строка " алвчрларплвод",
+//null - нулевое значения ( пуст)
+//undefined - не определенный  х .
+//boolean -булевой тип (чындык же жок)
+//symbol-символ ?*"&
+//bigint- узун маалыматтарды кыскартып берилгени ( 13234е4465)int-сан, big-ото чон
+
+
+//2-ссылочный тип (reference type)
+//object - {}
+
+
+
+//number type
+//745637653
+
+// variables -переменный - озгормолор
+//var p = 3.14
+// var бул эски аз колдонулат М:ФИО= бул  аты жонум (керекги жок)
+
+//let n = 45
+//let(- жариялоо )  аты n= 45  а мааниси 45 эгер 45 озгорткуь келсе мен дагы let  n=78 деп озгорто алам
+
+//let str // маани бербедим
+//const
+//const  birthYear = 1981 //озгорулгус
+    // ар бир переменныйдын аты уникалдуу кайталанбаасу шарт
+
+//console.log(p + n)
+//console.log(birthYear)
+//терминалды ачып  node app ( же  болбосо  файлдын аты кандай болсо ошону жазам)
+//console.log(str)
+
+// математикалык олераторлор
+
+// +
+// -
+//   / болуу
+// % калдык
+// *
+//  ** даража же степен
+
+//let a = 10
+//let  b =2
+//console.log(a*b)
+//console.log(a**b)
+//console.log(a%b)
+//console.log(a/b)
+
+// 7.99999  как округлять
+//Math.floor()ылдый
+//Math.ceil() жогору
+//Math.round()логоческий ,кайсыл жака жакын болсо ошого карап округлят этип берет
+//Math.trung()болчоксуз корсотот
+
+//let num = 7.99999
+//console.log(Math.floor(num))
+//console.log(Math.ceil(num))
+//console.log(Math.round(num))
+//console.log(Math.trunc(num))
+
+// Math.random() баш айланткан сан генерироват этуучу
+// сандардын диапазонун берип койом
+//console.log(Math.random() * 7) // каша э нерсе кабыл албайт
+//console.log(Math.floor(Math.random() * 7)) // каша э нерсе кабыл албайт
+
+// lotorea
+
+//let names =[ 'Tumar', 'Myrat','Azamat','Ayperi',"Sabrina"]
+
+//const idx = Math.floor(Math.random() * names. length)
+//console.log(names[idx])
+//console.log(Math.floor(Math.random() * 50)+1)
+//задания 10 шт
+//let n=67
+ //console.log(n)
+
+//let a=10
+//b=2
+ //c=5
+
+//console.log( a+b+c)
+
+
+//let  n=100
+//3 console.log ( n * 2 )
+
+//let n=67
+// b=7
+ //console.log(n % b)
+
+//let a=10
+//b=2
+// console.log(a + b)
+ //console.log(a - b)
+ //console.log(a * b)
+ //5 console.log(a / b)
+
+//let c=15
+//d=2
+ //let result= c + d
+ //6 console.log(result)
+
+//let a=30
+//d=15
+//c= a+d
+//7 console.log( c % 4)
+
+//let a=17
+//b=10
+//c = a - b
+//d =  5
+//let result = c + d
+//console.log(result)
+
+
+//let a='10'
+b='99'
+console.log()
+
+let names =[ "Ascar"]
+let number =10
+console.log(Ascar=10)
---
+++ strings.js
@@ -0,0 +1,88 @@
+//string -строка трмакчанын ичиндеги баардык нерсе 1 страка болот
+
+// 'hgdhjf[]hchdvb/.'
+//console.log("tumar")
+ let name="azamat"
+//console.log(name)
+//let country = "united states of america"
+// .length- узундук  прабелди да эсепке алат
+//[] -index - орун,сан
+//console.log(country[0])
+//console.log(country.length)
+// console.log(country[43])// мында 2 подход болуп калат
+console.log(country[country.length-1])//лучшый способ  и любой программада иштейт
+//console.log(country[Math.floor(country.length /2)])//ортодогу тамга же санды табуу
+
+//let company = "Googleoo"
+// indexof(g)  биринчи кездешуучу тамганы  корсотот (сан турундо)
+//lastIndexof(g) акыркы
+
+//console.log(company.indexOf("z"))//эгер сунуштаган тамга жок болсо анда косол -1 дегне сан корсотот
+//console.log(company.lastIndexOf("o"))
+
+
+//includes() составында эмне бар экенин текшерет ( соз турундо ооба же жок)
+
+//console.log(company.includes("a"))
+//console.log(company.includes("g"))
+
+//toUpperCase()
+//toLowerCase() кичине тамга
+//console.log(company.toUpperCase())
+//console.log(company.toLowerCase())
+
+//startWith()
+//endWith()
+//console.log(company.startsWith("G"))
+//console.log(company.endsWith("o"))
+
+//slice() вырез
+//substring()
+//substr()
+
+//console.log(company.) бут метотор чыгат
+//slice()
+//let username ="Almambetbek"
+//let surname="Trusbecov"
+//console.log(username. slice(0,8))
+//console.log(username. slice(0))
+//console.log(username. slice(6))
+//console.log(username. slice())
+//console.log(username. slice(-2))//aкырынан алдыгакарап санайт
+
+//substring()
+//console.log(username.substring(7))// slice бир туру эски бирок иштейт
+
+//substr()
+//console.log(username.substr(0,8))
+
+//repeat()
+//console.log(username.repeat(5))
+
+//trim() алды артынан бош жерди алып так санды гана чыгарат
+//console.log(     " Tumar "   .length)
+//console.log(        "48786786740 " .trim())
+
+//concatenation  2 tecsti koshyy
+//console.log(username + "" + surname)
+//console.log(username.concat(surname))
+//console.log(username.charAt(7))
+//console.log(username.replace("A","O"))//альаштыруу
+//console.log(username.bold())
+//console.log(username.italics())
+//console.log(username.link("https://google.com"))
+
+
+//шаблоный летерал
+
+//backtick ``
+//let name  = "Tumar "
+//let floor = 7
+//let country ="KG"
+   // console.log(`Hello ${name}, he lives on the ${floor} th floor,he is from ${country}` )
+
+//let text='Привет мир'
+
+//console.log( text)
+let text= 'Hello,'
+//let name= 'World'
